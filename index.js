dd5aea957283db5155b6037823ac65b0a509760a
Skillzya <yasstoure092@gmail.com>
2026-02-26 13:55:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+// Pour lire les POST JSON si tu envoies le formulaire via fetch
+app.use(express.json());
+
+// Servir les fichiers statiques (HTML/CSS/JS)
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3000;
 
+// Route test
 app.get("/", (req, res) => {
-  res.send("Serveur en ligne");
+  res.sendFile(__dirname + "/index.html"); // ou juste "Serveur en ligne" si tu veux tester
 });
 
 app.listen(PORT, () => {
